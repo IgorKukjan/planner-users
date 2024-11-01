4b03f9ddc1e3c5de720111369a19aada1fd45b5f
Igor Kukyan <kukyan@irc.beltelecom.by>
2024-11-01 14:27:36 +0300

    54 55 56 planner-users

--- src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.java
+++ src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.java
@@ -3,9 +3,15 @@ package ru.javabegin.micro.planner.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//с каких пакетов начинать искать spring beans
+@ComponentScan(basePackages = {"ru.javabegin.micro.planner"})
+//шде искать jpa repositories
+@EnableJpaRepositories(basePackages = {"ru.javabegin.micro.planner.users"})
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
