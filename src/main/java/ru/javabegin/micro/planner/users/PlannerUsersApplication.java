159ae2eef8e8205ed59aa8e8c13ecda399988730
Igor Kukyan <kukyan@irc.beltelecom.by>
2025-01-13 09:21:13 +0300

    83 Динамическое обновление настроек

--- src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.java
+++ src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.java
@@ -3,6 +3,7 @@ package ru.javabegin.micro.planner.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,6 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"ru.javabegin.micro.planner"})
 //шде искать jpa repositories
 @EnableJpaRepositories(basePackages = {"ru.javabegin.micro.planner.users"})
+//динамически изменяет состав бина(динамически считает новые значения из properties)
+@RefreshScope
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
