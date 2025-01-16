8e33949416629b751e7ba42d91f235e57e282e91
Igor Kukyan <kukyan@irc.beltelecom.by>
2025-01-16 16:41:40 +0300

    88 Подключение Kotlin к проекту planner

--- src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.kt
+++ src/main/java/ru/javabegin/micro/planner/users/PlannerUsersApplication.kt
@@ -1,24 +1,20 @@
-package ru.javabegin.micro.planner.users;
+package ru.javabegin.micro.planner.users
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-@EnableDiscoveryClient
-//с каких пакетов начинать искать spring beans
-@ComponentScan(basePackages = {"ru.javabegin.micro.planner"})
-//шде искать jpa repositories
-@EnableJpaRepositories(basePackages = {"ru.javabegin.micro.planner.users"})
-//динамически изменяет состав бина(динамически считает новые значения из properties)
+@EnableDiscoveryClient //с каких пакетов начинать искать spring beans
+@ComponentScan(basePackages = ["ru.javabegin.micro.planner"]) //шде искать jpa repositories
+@EnableJpaRepositories(basePackages = ["ru.javabegin.micro.planner.users"]) //динамически изменяет состав бина(динамически считает новые значения из properties)
 @RefreshScope
-public class PlannerUsersApplication {
+open class  PlannerUsersApplication
 
-    public static void main(String[] args) {
-        SpringApplication.run(PlannerUsersApplication.class, args);
-    }
-
+fun main(args: Array<String>) {
+    runApplication<PlannerUsersApplication>(*args)
 }
+
