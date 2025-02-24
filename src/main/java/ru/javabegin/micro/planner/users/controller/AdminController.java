88d4d98aacdfda4f50082b94b3b15e12f29f97cc
Igor Kukyan <kukyan@irc.beltelecom.by>
2025-02-24 15:36:33 +0300

    work

--- src/main/java/ru/javabegin/micro/planner/users/controller/AdminController.java
+++ src/main/java/ru/javabegin/micro/planner/users/controller/AdminController.java
@@ -1,5 +1,7 @@
 package ru.javabegin.micro.planner.users.controller;
 
+import jakarta.ws.rs.core.Response;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,12 +10,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.javabegin.micro.planner.entity.User;
+import ru.javabegin.micro.planner.users.dto.UserDTO;
+import ru.javabegin.micro.planner.users.keycloak.KeycloakUtils;
 import ru.javabegin.micro.planner.users.mq.func.MessageFuncActions;
 import ru.javabegin.micro.planner.users.search.UserSearchValues;
 import ru.javabegin.micro.planner.users.service.UserService;
 import ru.javabegin.micro.planner.utils.rest.webclient.UserWebClientBuilder;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -38,44 +44,48 @@ public class AdminController {
 
     public static final String ID_COLUMN = "id"; // имя столбца id
     private final UserService userService; // сервис для доступа к данным (напрямую к репозиториям не обращаемся)
+    private final KeycloakUtils keycloakUtils;
+
     private UserWebClientBuilder userWebClientBuilder;
     private MessageFuncActions messageFuncActions;
 
 
+
     // используем автоматическое внедрение экземпляра класса через конструктор
     // не используем @Autowired ля переменной класса, т.к. "Field injection is not recommended "
-    public AdminController(UserService userService, UserWebClientBuilder userWebClientBuilder, MessageFuncActions messageFuncActions) {
+    public AdminController(UserService userService, UserWebClientBuilder userWebClientBuilder, MessageFuncActions messageFuncActions, KeycloakUtils keycloakUtils) {
         this.userService = userService;
         this.userWebClientBuilder = userWebClientBuilder;
         this.messageFuncActions = messageFuncActions;
+        this.keycloakUtils = keycloakUtils;
     }
 
 
     // добавление
     @PostMapping("/add")
-    public ResponseEntity<User> add(@RequestBody User user) {
+    public ResponseEntity add(@RequestBody UserDTO userDTO) {
 
         // проверка на обязательные параметры
-        if (user.getId() != null && user.getId() != 0) {
+        if (userDTO.getId() != null) {
             // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
             return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
         }
 
         // если передали пустое значение
-        if (user.getEmail() == null || user.getEmail().trim().length() == 0) {
+        if (userDTO.getEmail() == null || userDTO.getEmail().trim().length() == 0) {
             return new ResponseEntity("missed param: email", HttpStatus.NOT_ACCEPTABLE);
         }
 
-        if (user.getPassword() == null || user.getPassword().trim().length() == 0) {
+        if (userDTO.getPassword() == null || userDTO.getPassword().trim().length() == 0) {
             return new ResponseEntity("missed param: password", HttpStatus.NOT_ACCEPTABLE);
         }
 
-        if (user.getUsername() == null || user.getUsername().trim().length() == 0) {
+        if (userDTO.getUsername() == null || userDTO.getUsername().trim().length() == 0) {
             return new ResponseEntity("missed param: username", HttpStatus.NOT_ACCEPTABLE);
         }
 
 
-        user = userService.add(user);
+//        user = userService.add(user);
 
 //        if(user != null){
 //            userWebClientBuilder.initUserData(user.getId()).subscribe(result ->{
@@ -83,38 +93,47 @@ public class AdminController {
 //            });
 //        }
 
-        messageFuncActions.sendNewUserMessage(user.getId());
+//        messageFuncActions.sendNewUserMessage(user.getId());
 
-        return ResponseEntity.ok(user); // возвращаем созданный объект со сгенерированным id
+        List<String> defaultRoles = new ArrayList<>();
+        defaultRoles.add("user");
+        defaultRoles.add("admin");
+
+        Response createResponse = keycloakUtils.createKeycloakUser(userDTO, defaultRoles);
+
+        return ResponseEntity.status(createResponse.getStatus()).build(); // возвращаем созданный объект со сгенерированным id
 
     }
 
 
     // обновление
     @PutMapping("/update")
-    public ResponseEntity<User> update(@RequestBody User user) {
+    public ResponseEntity update(@RequestBody UserDTO userDTO) {
 
         // проверка на обязательные параметры
-        if (user.getId() == null || user.getId() == 0) {
+        if (userDTO.getId().isBlank()) {
             return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
         }
 
         // если передали пустое значение
-        if (user.getEmail() == null || user.getEmail().trim().length() == 0) {
-            return new ResponseEntity("missed param: email", HttpStatus.NOT_ACCEPTABLE);
-        }
+//        if (userDTO.getEmail() == null || userDTO.getEmail().trim().length() == 0) {
+//            return new ResponseEntity("missed param: email", HttpStatus.NOT_ACCEPTABLE);
+//        }
 
-        if (user.getPassword() == null || user.getPassword().trim().length() == 0) {
-            return new ResponseEntity("missed param: password", HttpStatus.NOT_ACCEPTABLE);
-        }
+//        if (userDTO.getPassword() == null || userDTO.getPassword().trim().length() == 0) {
+//            return new ResponseEntity("missed param: password", HttpStatus.NOT_ACCEPTABLE);
+//        }
 
-        if (user.getUsername() == null || user.getUsername().trim().length() == 0) {
-            return new ResponseEntity("missed param: username", HttpStatus.NOT_ACCEPTABLE);
-        }
+//        if (userDTO.getUsername() == null || userDTO.getUsername().trim().length() == 0) {
+//            return new ResponseEntity("missed param: username", HttpStatus.NOT_ACCEPTABLE);
+//        }
 
 
         // save работает как на добавление, так и на обновление
-        userService.update(user);
+//        userService.update(userDTO);
+
+        // save работает как на добавление, так и на обновление
+        keycloakUtils.updateKeycloakUser(userDTO);
 
         return new ResponseEntity(HttpStatus.OK); // просто отправляем статус 200 (операция прошла успешно)
 
@@ -123,16 +142,19 @@ public class AdminController {
 
     // для удаления используем типа запроса put, а не delete, т.к. он позволяет передавать значение в body, а не в адресной строке
     @PostMapping("/deletebyid")
-    public ResponseEntity deleteByUserId(@RequestBody Long userId) {
+    public ResponseEntity deleteByUserId(@RequestBody String userId) {
 
         // можно обойтись и без try-catch, тогда будет возвращаться полная ошибка (stacktrace)
         // здесь показан пример, как можно обрабатывать исключение и отправлять свой текст/статус
-        try {
-            userService.deleteByUserId(userId);
-        } catch (EmptyResultDataAccessException e) {
-            e.printStackTrace();
-            return new ResponseEntity("userId=" + userId + " not found", HttpStatus.NOT_ACCEPTABLE);
-        }
+//        try {
+//            userService.deleteByUserId(userId);
+//        } catch (EmptyResultDataAccessException e) {
+//            e.printStackTrace();
+//            return new ResponseEntity("userId=" + userId + " not found", HttpStatus.NOT_ACCEPTABLE);
+//        }
+
+        keycloakUtils.deleteKeycloakUser(userId);
+
         return new ResponseEntity(HttpStatus.OK); // просто отправляем статус 200 (операция прошла успешно)
     }
 
@@ -171,22 +193,24 @@ public class AdminController {
 //    }
 
     @PostMapping("/id")
-    public ResponseEntity<User> findById(@RequestBody Long id) {
+    public ResponseEntity<UserRepresentation> findById(@RequestBody String userId) {
 
-        Optional<User> userOptional = userService.findById(id);
+//        Optional<User> userOptional = userService.findById(id);
 
         // можно обойтись и без try-catch, тогда будет возвращаться полная ошибка (stacktrace)
         // здесь показан пример, как можно обрабатывать исключение и отправлять свой текст/статус
-        try {
-            if (userOptional.isPresent()) { // если объект найден
-                return ResponseEntity.ok(userOptional.get()); // получаем User из контейнера и возвращаем в теле ответа
-            }
-        } catch (NoSuchElementException e) { // если объект не будет найден
-            e.printStackTrace();
-        }
+//        try {
+//            if (userOptional.isPresent()) { // если объект найден
+//                return ResponseEntity.ok(userOptional.get()); // получаем User из контейнера и возвращаем в теле ответа
+//            }
+//        } catch (NoSuchElementException e) { // если объект не будет найден
+//            e.printStackTrace();
+//        }
 
         // пользователь с таким id не найден
-        return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+//        return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+
+        return ResponseEntity.ok(keycloakUtils.findUserById(userId));
     }
 
     // получение уникального объекта по email
@@ -211,28 +235,28 @@ public class AdminController {
 
     // поиск по любым параметрам UserSearchValues
     @PostMapping("/search")
-    public ResponseEntity<Page<User>> search(@RequestBody UserSearchValues userSearchValues) throws ParseException {
+    public ResponseEntity<List<UserRepresentation>> search(@RequestBody String email) throws ParseException {
 
         // все заполненные условия проверяются условием ИЛИ - это можно изменять в запросе репозитория
 
         // можно передавать не полный email, а любой текст для поиска
-        String email = userSearchValues.getEmail() != null ? userSearchValues.getEmail() : null;
+//        String email = userSearchValues.getEmail() != null ? userSearchValues.getEmail() : null;
 
-        String username = userSearchValues.getUsername() != null ? userSearchValues.getUsername() : null;
+//        String username = userSearchValues.getUsername() != null ? userSearchValues.getUsername() : null;
 
 //        // проверка на обязательные параметры - если они нужны по задаче
 //        if (email == null || email.trim().length() == 0) {
 //            return new ResponseEntity("missed param: user email", HttpStatus.NOT_ACCEPTABLE);
 //        }
 
-        String sortColumn = userSearchValues.getSortColumn() != null ? userSearchValues.getSortColumn() : null;
-        String sortDirection = userSearchValues.getSortDirection() != null ? userSearchValues.getSortDirection() : null;
+//        String sortColumn = userSearchValues.getSortColumn() != null ? userSearchValues.getSortColumn() : null;
+//        String sortDirection = userSearchValues.getSortDirection() != null ? userSearchValues.getSortDirection() : null;
 
-        Integer pageNumber = userSearchValues.getPageNumber() != null ? userSearchValues.getPageNumber() : null;
-        Integer pageSize = userSearchValues.getPageSize() != null ? userSearchValues.getPageSize() : null;
+//        Integer pageNumber = userSearchValues.getPageNumber() != null ? userSearchValues.getPageNumber() : null;
+//        Integer pageSize = userSearchValues.getPageSize() != null ? userSearchValues.getPageSize() : null;
 
         // направление сортировки
-        Sort.Direction direction = sortDirection == null || sortDirection.trim().length() == 0 || sortDirection.trim().equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+//        Sort.Direction direction = sortDirection == null || sortDirection.trim().length() == 0 || sortDirection.trim().equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
 
         /* Вторым полем для сортировки добавляем id, чтобы всегда сохранялся строгий порядок.
             Например, если у 2-х задач одинаковое значение приоритета и мы сортируем по этому полю.
@@ -241,16 +265,16 @@ public class AdminController {
          */
 
         // объект сортировки, который содержит стобец и направление
-        Sort sort = Sort.by(direction, sortColumn, ID_COLUMN);
+//        Sort sort = Sort.by(direction, sortColumn, ID_COLUMN);
 
         // объект постраничности
-        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
+//        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
 
         // результат запроса с постраничным выводом
-        Page<User> result = userService.findByParams(email, username, pageRequest);
+//        Page<User> result = userService.findByParams(email, username, pageRequest);
 
         // результат запроса
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(keycloakUtils.searchKeycloakUsers(email));
 
     }
 
--- src/main/java/ru/javabegin/micro/planner/users/keycloak/KeycloakUtils.java
+++ src/main/java/ru/javabegin/micro/planner/users/keycloak/KeycloakUtils.java
@@ -5,17 +5,24 @@ import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import ru.javabegin.micro.planner.entity.User;
+import ru.javabegin.micro.planner.users.dto.UserDTO;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class KeycloakUtils {
+
+    private static final int CONFLICT = 409; // если пользователь уже существует в KC и пытаемся создать такого же
 
     @Value("${keycloak.auth-server-url}")
     private String serverUrl;
@@ -47,30 +54,63 @@ public class KeycloakUtils {
     }
 
 
-    //создание пользователя для кс
-    public Response createKeycloakUser(User user) {
+//    //создание пользователя для кс
+//    public Response createKeycloakUser(UserDTO userDTO, List<String> roles) {
+//
+//        //доступ к api realm
+//        RealmResource realmsResource = getInstance().realm(realm); //todoapp-realm
+//
+//        //доступ к api для работы с пользователями
+//        UsersResource usersResource = realmsResource.users();
+//
+//        CredentialRepresentation credentialRepresentation = createPasswordCredentials(userDTO.getPassword());
+//
+//
+//        UserRepresentation kcUser = new UserRepresentation();
+//
+//        kcUser.setUsername(userDTO.getUsername());
+//        kcUser.setCredentials(Collections.singletonList(credentialRepresentation));
+//        kcUser.setEmail(userDTO.getEmail());
+//        kcUser.setEnabled(true);//пользователь задействован сразу
+//        kcUser.setEmailVerified(false);
 
-        //доступ к api realm
-        RealmResource realmsResource = getInstance().realm(realm); //todoapp-realm
+    /// /        kcUser.setRealmRoles(roles);
+//
+//        Response response = usersResource.create(kcUser);
+//
+//        return response;
+//    }
+    public Response createKeycloakUser(UserDTO userDTO, List<String> roles) {
 
-        //доступ к api для работы с пользователями
-        UsersResource usersResource = realmsResource.users();
+        UserRepresentation user = new UserRepresentation();
+        user.setEnabled(true);
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+//        user.setFirstName(userDTO.getFirstName());
+//        user.setLastName(userDTO.getLastName());
+        user.setEmailVerified(false);
 
-        CredentialRepresentation credentialRepresentation = createPasswordCredentials(user.getPassword());
+        CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+        credentialRepresentation.setValue(userDTO.getPassword());
+        credentialRepresentation.setTemporary(false);
+        credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
 
+        List<CredentialRepresentation> list = new ArrayList<>();
+        list.add(credentialRepresentation);
+        user.setCredentials(list);
 
-        UserRepresentation kcUser = new UserRepresentation();
+        UsersResource usersResource = getUsersResource();
 
-        kcUser.setUsername(user.getEmail());
-        kcUser.setCredentials(Collections.singletonList(credentialRepresentation));
-        kcUser.setEmail(user.getEmail());
-        kcUser.setEnabled(true);//пользователь задействован сразу
-        kcUser.setEmailVerified(false);
-
-        Response response = usersResource.create(kcUser);
+        Response response = usersResource.create(user);
 
         return response;
     }
+
+    private UsersResource getUsersResource() {
+        RealmResource realm1 =  getInstance().realm(realm);
+        return realm1.users();
+    }
+
 
     //создание пароля для пользователя
     private CredentialRepresentation createPasswordCredentials(String password) {
@@ -83,4 +123,103 @@ public class KeycloakUtils {
         return passwordCredentials;
     }
 
+    // поиск уникального пользователя
+    public UserRepresentation findUserById(String userId) {
+        RealmResource realmsResource = getInstance().realm(realm);
+        UsersResource usersResource = realmsResource.users();
+
+        // получаем пользователя
+        return usersResource.get(userId).toRepresentation();
+    }
+
+    // поиск пользователя по любым атрибутам (вхождение текста)
+    public List<UserRepresentation> searchKeycloakUsers(String text) {
+        RealmResource realmsResource = getInstance().realm(realm);
+        UsersResource usersResource = realmsResource.users();
+
+        // получаем пользователя
+        return usersResource.searchByAttributes(text);
+    }
+
+
+    // обновление пользователя для KC
+//    public void updateKeycloakUser(UserDTO userDTO) {
+//        RealmResource realmsResource = getInstance().realm(realm);
+//        UsersResource usersResource = realmsResource.users();
+//
+//        // данные пароля - специальный объект-контейнер CredentialRepresentation
+//        CredentialRepresentation credentialRepresentation = createPasswordCredentials(userDTO.getPassword());
+//
+//        // какие поля обновляем
+//        UserRepresentation kcUser = new UserRepresentation();
+//        kcUser.setUsername(userDTO.getUsername());
+//        kcUser.setCredentials(Collections.singletonList(credentialRepresentation));
+//        kcUser.setEmail(userDTO.getEmail());
+//
+//        // получаем пользователя
+//        UserResource uniqueUserResource = usersResource.get(userDTO.getId());
+//        uniqueUserResource.update(kcUser); // обновление
+//    }
+
+//    public void updateKeycloakUser(UserDTO userDTO) {
+//
+//        UserRepresentation user = new UserRepresentation();
+////        user.setEnabled(true);
+//        user.setUsername(userDTO.getUsername());
+//        user.setEmail(userDTO.getEmail());
+////        user.setFirstName(userDTO.getFirstName());
+////        user.setLastName(userDTO.getLastName());
+////        user.setEmailVerified(false);
+//
+//        CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+//        credentialRepresentation.setValue(userDTO.getPassword());
+//        credentialRepresentation.setTemporary(false);
+//        credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+//
+//        List<CredentialRepresentation> list = new ArrayList<>();
+//        list.add(credentialRepresentation);
+//        user.setCredentials(list);
+//
+//        UsersResource usersResource = getUsersResource();
+//
+//        // получаем пользователя
+//        UserResource uniqueUserResource = usersResource.get(userDTO.getId());
+//        uniqueUserResource.update(user); // обновление
+//    }
+
+
+    // удаление пользователя для KC
+    public void deleteKeycloakUser(String userId) {
+        RealmResource realmsResource = getInstance().realm(realm);
+        UsersResource usersResource = realmsResource.users();
+
+        // получаем пользователя
+        UserResource uniqueUserResource = usersResource.get(userId);
+        uniqueUserResource.remove();
+
+    }
+
+    public void updateKeycloakUser(UserDTO userDTO) {
+
+
+        UserResource userResource = getUserResource(userDTO.getId());
+        CredentialRepresentation credentialRepresentation=new CredentialRepresentation();
+        credentialRepresentation.setValue(userDTO.getPassword());
+        credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+        credentialRepresentation.setTemporary(false);
+
+
+        UserRepresentation kcUser = new UserRepresentation();
+//        kcUser.setUsername(userDTO.getUsername());
+        kcUser.setCredentials(Collections.singletonList(credentialRepresentation));
+        kcUser.setEmail(userDTO.getEmail());
+
+        userResource.update(kcUser);
+    }
+
+
+    public UserResource getUserResource(String userId){
+        UsersResource usersResource = getUsersResource();
+        return usersResource.get(userId);
+    }
 }
